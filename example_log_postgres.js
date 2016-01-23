b6fe33a70250decda1963cab4242cec3855136c9
Michallis Pashidis <michallis@trust1tream.com>
2016-01-23 17:39:28 +0100

    update console log

--- example_log_postgres.js
+++ example_log_postgres.js
@@ -1,4 +1,5 @@
 var alog = require('./lib/auditlog');
 
-alog.addTransport("postgres", {connectionString:"postgres://postgres:postgres@localhost:5433/digi_vault_ui_api", debug:true});
+alog.addTransport("postgres", {connectionString:"postgres://postgres:postgres@localhost:5433/digi_vault_ui_api", debug:false});
+alog.addTransport("console");
 alog.logEvent( '100', 'origin', 'someaction', 'somelabel', '{obj:value}', 'desc');
